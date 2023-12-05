907282ef478db40ce28a0cb1d5fe48ae12bf65b3
Angelo Andres Olmedo C. <aaolmedo2@espe.edu.ec>
2023-12-05 11:22:50 -0500

    act-8

---
+++ act-8/reverse.cpp
@@ -0,0 +1,11 @@
+int reversenumber(int n)
+{
+   int reverse = 0, rem;
+   while (n != 0)
+   {
+      rem = n % 10;
+      reverse = reverse * 10 + rem;
+      n /= 10;
+   }
+   return reverse;
+}
